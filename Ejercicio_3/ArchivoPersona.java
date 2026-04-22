fd64923df2dc4d5952b32850f8524984d0757ba4
JavoLuna <lalosieg@gmail.com>
2026-04-22 13:48:43 -0300

    feat: agregar metodos
    buscar(), mostrar(), promedio(), alturaMayorA(), comprobarAltura()

--- Ejercicio_3/ArchivoPersona.java
+++ Ejercicio_3/ArchivoPersona.java
@@ -1,10 +1,14 @@
-package Ejercicio_3;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
 
 public class ArchivoPersona {
     RandomAccessFile archivo;
+    private final static int tamNom=17;
+    private final static int tamDni=4;
+    private final static int tamAlt=4;
+    private final static int tamTotal=25;
 
     public ArchivoPersona() {
         try {
@@ -23,7 +27,7 @@ public class ArchivoPersona {
                 archivo.writeUTF(String.format("%-15s",p.getNombre()));
             }
             archivo.writeInt(p.getDni());
-            archivo.writeDouble(p.getAltura());
+            archivo.writeInt(p.getAltura());
         } catch (IOException e) {
             System.out.println("Error al agregar" + e.getMessage());
         }
@@ -39,7 +43,7 @@ public class ArchivoPersona {
                 while(archivo.getFilePointer()<archivo.length()){
                     System.out.println("Nombre: " + archivo.readUTF());
                     System.out.println("DNI: " + archivo.readInt());
-                    System.out.println("Altura: " + archivo.readDouble());
+                    System.out.println("Altura: " + archivo.readInt());
                 }
             }
         }catch(IOException e){
@@ -53,5 +57,99 @@ public class ArchivoPersona {
         }catch(IOException e){
             System.out.println("Error al cerrar" + e.getMessage());
         }
+    }
+
+    public long buscar(int dni){
+        long aux = -1;
+        
+        if (dni<0) {
+            System.out.println("No existe ese DNI");
+        }else{
+            try{
+                if (archivo.length()==0) {
+                    System.out.println("El arhivo está vacio");
+                }else{
+                    archivo.seek(tamNom);
+                    boolean encontrado = false;
+                    while (archivo.getFilePointer() <= archivo.length()&& !encontrado){
+                        if(dni==archivo.readInt()){
+                            aux=archivo.getFilePointer()-(tamDni+tamNom);
+                            encontrado=true;
+                        }else{
+                            archivo.skipBytes(tamAlt+tamNom);
+                        }
+                        
+                    }
+                }
+            }catch(IOException e){
+                System.out.println("Error al buscar: " + e.getMessage());
+            }
+        }
+        return aux;
+
+    }
+
+    public void mostrar(long posicion){
+        if (posicion>0) {
+            try{
+            archivo.seek(posicion);
+            System.out.println("Nombre: " + archivo.readUTF());
+            System.out.println("DNI: " + archivo.readInt());
+            System.out.println("Altura: " + archivo.readInt());
+            }catch(IOException e){
+                System.out.println("Error al mostrar: "+e.getMessage());
+            }
+        }
+    }
+
+    public double promedio(){
+        int suma=0;
+        int contador=0;
+        try{
+            archivo.seek(tamNom+tamDni);
+            while (archivo.getFilePointer()+(tamNom+tamDni)<=archivo.length()){
+                int valorAltura=archivo.readInt();
+                if (valorAltura!=0) {
+                    suma+=valorAltura;
+                    contador++;
+                }
+                archivo.skipBytes(tamNom+tamDni);
+            }
+        }catch (IOException e){
+            System.out.println("Error al calcular: " + e.getMessage());
+        }
+        return (double)suma/contador;
+    }
+
+    public RandomAccessFile alturaMayorA(){
+        RandomAccessFile auxiliar = null;
+        try {
+            auxiliar = new RandomAccessFile("auxiliar.dat", "rw");
+            archivo.seek(tamNom+tamDni);
+            while (archivo.getFilePointer()+tamNom+tamDni<=archivo.length()) {
+                if (archivo.readInt()>160) {
+                    archivo.skipBytes(-(tamTotal));
+                    auxiliar.writeUTF(archivo.readUTF());
+                    auxiliar.writeInt(archivo.readInt());
+                    auxiliar.writeInt(archivo.readInt());
+                }
+                archivo.skipBytes(tamNom+tamDni);
+            }
+        } catch (IOException e) {
+            System.out.println("No se pudo calcular:" + e.getMessage());
+        }
+        return auxiliar;
+    }
+
+    public void comprobarAltura(RandomAccessFile auxiliar){
+        try{
+            if (auxiliar.length()>0){
+                System.out.println("Archivo creado exitosamente");
+                auxiliar.close();
+            }
+        }catch(IOException e){
+            System.out.println("Error al comprobar: " + e.getMessage());
+        }
+        
     }
 }
